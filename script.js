61852cca1fdd5baaa8e937fee57492dfacf696c6
cyberrebyc1-ops <cyberrebyc1@gmail.com>
2026-03-13 09:28:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('Hello');
+
+async function generate(){
+
+let text = document.getElementById("text").value;
+
+let response = await fetch(
+"https://api-inference.huggingface.co/models/YOUR_MODEL",
+{
+method:"POST",
+headers:{
+"Authorization":"Bearer YOUR_TOKEN",
+"Content-Type":"application/json"
+},
+body:JSON.stringify({inputs:text})
+}
+);
+
+let audioBlob = await response.blob();
+
+let audioURL = URL.createObjectURL(audioBlob);
+
+document.getElementById("audio").src = audioURL;
+
+}
